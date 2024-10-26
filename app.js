464d64a60a58490d0de9850df00806becf7a92d4
P-vashu <135582400+P-vashu@users.noreply.github.com>
2024-10-27 00:15:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoStore = require('connect-mongo');
 // const dbUrl = process.env.DB_URL;
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+// const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 // 'mongodb://localhost:27017/yelp-camp'
-mongoose.connect(dbUrl, {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
